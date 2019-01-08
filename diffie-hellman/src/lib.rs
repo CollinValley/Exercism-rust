aa5c8baec467584bde5b67ec9ee82afc506f11fb
Collin Valley <collin.valley@gmail.com>
2019-01-07 22:45:01 -0800

    Add and complete diffie-helman, remove old saddle-points

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -1,21 +1,34 @@
+extern crate rand;
+
+use rand::Rng;
+
 pub fn private_key(p: u64) -> u64 {
-    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    let mut rng = rand::thread_rng();
+    rng.gen_range(2, p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate public key using prime numbers {} and {}, and private key {}",
-        p,
-        g,
-        a
-    )
+    mod_exp(g, a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate secret key using prime number {}, public key {}, and private key {}",
-        p,
-        b_pub,
-        a
-    )
+    mod_exp(b_pub, a, p)
+}
+
+//LOL rollin my own crypto
+fn mod_exp(mut base: u64, mut exp: u64, modulus: u64) -> u64 {
+    let mut result = 1;
+    if modulus == 1 {
+        0
+    } else {
+        base = base % modulus;
+        while exp > 0 {
+            if (exp & 1) == 1 {
+                result = (result * base) % modulus;
+            }
+            exp = exp >> 1;
+            base = (base * base) % modulus;
+        }
+        result
+    }
 }
