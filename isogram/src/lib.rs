b78a53973b713651c5847d38403d148859acab56
Collin Valley <collin.valley@gmail.com>
2019-01-14 21:43:42 -0800

    Add solution for isograms

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -1,3 +1,13 @@
+use std::collections::HashSet;
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    let mut seen_chars = HashSet::new();
+    for letter in candidate.chars().filter(|c| c.is_alphanumeric()) {
+        for c in letter.to_lowercase() {
+            if seen_chars.contains(&c) {
+                return false
+            }
+                seen_chars.insert(c);
+        }
+    }
+    true
 }
